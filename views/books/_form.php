c1da3abea41a35d5a34aac63349cabe05de333b7
Merfilde <best.naked@gmail.com>
2019-02-11 12:37:19 +0300

    create views for CRUD

---
+++ views/books/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use kartik\select2\Select2;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Books */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="books-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'price')->textInput() ?>
+
+    <?= $form->field($model, 'author_id')->widget(Select2::classname(), [
+        'data' => app\models\Authors::getAllAuthors(),
+        'options' => ['placeholder' => 'Select a author ...'],
+        'pluginOptions' => [
+            'allowClear' => true
+        ],
+    ]); ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+
+</div>
